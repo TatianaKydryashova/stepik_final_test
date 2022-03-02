106d69b3debdd836372702e9c174bc37bdc15b48
TatianaKydryashova <kudryashova_ts@mail.ru>
2022-03-02 10:51:37 +0300

    add parametrize test

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,13 @@
 import time
 
+import pytest
+
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', [*range(1, 7), pytest.param(7, marks=pytest.mark.xfail), *range(8, 10)])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     page = ProductPage(browser, link, 10)
     page.open()
     page.add_to_basket()
